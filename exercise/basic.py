de4b0dbc51dfd45175de5df685c37d32519b2348
letYuchan <142238082+letYuchan@users.noreply.github.com>
2024-09-12 15:46:33 +0900

    python1

---
+++ exercise/basic.py
@@ -0,0 +1,35 @@
+print("hello world!")
+print(3.29)
+# int
+a = 329
+
+# float
+a = 3. # == 3.0
+b = 3.29
+
+# bool
+c = False
+if(c == 0):
+    print("같습니다")
+else:
+    print("다릅니다")
+
+#string
+name = "cho"
+multiLineStr = "Yuchan\nCho"
+multiLineSTrDifferentVer = """Yuchan
+cho"""
+print(multiLineSTrDifferentVer)
+
+#compound data
+# tuple, list, set, dictionary
+prof_tuple = ("Cho", 30, False)
+print(prof_tuple)
+prof_list = ["Cho", 30, False]
+print(prof_list)
+prof_set = {"Cho", 30, False}
+print(prof_set)
+prof_dict = {"name" : "Cho", "age" : 30, "isStudent" : False}
+print(prof_dict)
+
+
